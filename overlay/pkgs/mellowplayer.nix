dfc02e8dd4edb0c4fd35406c09f4857ec4965bb3
Petr Kozorezov <petr.kozorezov@gmail.com>
2020-02-21 19:58:27 +0300

    update mellow player

--- overlay/pkgs/mellowplayer.nix
+++ overlay/pkgs/mellowplayer.nix
@@ -3,7 +3,6 @@
 stdenv.mkDerivation rec {
   pname   = "mellowplayer";
   version = "3.5.10";
-  # TODO application item
 
   src = fetchFromGitLab {
     owner  = "ColinDuquesnoy";
@@ -12,24 +11,16 @@ stdenv.mkDerivation rec {
     sha256 = "1qyzswsls6hkyvmxp7z3wjkjg8vq01vwnq4v4n6aiyna4dqgki6a";
   };
 
-  nativeBuildInputs = [ cmake wrapQtAppsHook qttools ];
-  buildInputs = [ qtbase qtsvg qtquickcontrols2 qtwebengine qtgraphicaleffects pkgconfig xorg.libX11 gdk-pixbuf libnotify ];
-
-  # qtdeclarative
-  #propagatedBuildInputs = [ qt5.qtdeclarative ];
-
-  #checkPhase = ''
-  #  export QT_PLUGIN_PATH="${qt5.qtbase.bin}/${qt5.qtbase.qtPluginPrefix}"
-  #  echo "$QT_PLUGIN_PATH"
-  #'';
+  nativeBuildInputs = [ cmake wrapQtAppsHook pkgconfig ];
+  buildInputs = [ qtbase qttools qtsvg qtquickcontrols2 qtwebengine qtgraphicaleffects xorg.libX11 gdk-pixbuf libnotify ];
 
   enableParallelBuilding = true;
 
   meta = {
     homepage    = https://colinduquesnoy.gitlab.io/MellowPlayer/;
     description = "MellowPlayer is a desktop application that runs web-based music streaming services";
-    license     = stdenv.lib.licenses.gpl2; # TODO
+    license     = stdenv.lib.licenses.gpl2;
     maintainers = with stdenv.lib.maintainers; [ ]; # TODO
-    platforms   = with stdenv.lib.platforms; linux; # TODO
+    platforms   = with stdenv.lib.platforms; linux;
   };
 }
