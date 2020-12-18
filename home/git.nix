d9eabf6fee6e48b139c4ddf5b5c4120a4bc2007f
Petr Kozorezov <petr.kozorezov@gmail.com>
2020-12-19 00:09:52 +0300

    update git

--- home/git.nix
+++ home/git.nix
@@ -1,23 +1,30 @@
 { pkgs, ... }:
 {
-  programs.git = {
-    enable    = true;
-    userEmail = "petr.kozorezov@gmail.com";
-    userName  = "Petr Kozorezov";
-    signing   = {
-      key           = "EF2A246DDE509B0C";
-      signByDefault = true;
+  programs = {
+    git = {
+      enable    = true;
+      userEmail = "petr.kozorezov@gmail.com";
+      userName  = "Petr Kozorezov";
+      signing   = {
+        key           = "EF2A246DDE509B0C";
+        signByDefault = true;
+      };
+      extraConfig = {
+        push.default = "current";
+        pull.ff      = "only";
+      };
     };
-    extraConfig = {
-      push.default = "current";
-      pull.ff      = "only";
+    gh = {
+      enable      = true;
+      gitProtocol = "ssh";
+      # TODO
+      #aliases = {};
     };
   };
 
+
   home.packages = with pkgs;
     [
-      gitAndTools.hub
-      gitAndTools.gh
       glow
       git-crypt
       gitAndTools.git-filter-repo
