b0920aac543d15b29aefea90246a43adf89890b6
Petr Kozorezov <petr.kozorezov@gmail.com>
2021-01-05 18:54:55 +0300

    add installer

---
+++ installer/iso.nix
@@ -0,0 +1,22 @@
+{ pkgs, ... }:
+{
+  systemd.services.sshd.wantedBy = pkgs.lib.mkForce [ "multi-user.target" ];
+
+  users = {
+    mutableUsers = false;
+    users.root.openssh.authorizedKeys.keys = [ (builtins.readFile ../secrets/ssh.key.pub) ];
+  };
+
+  environment.etc = {
+    # TODO move to the better place
+    "installer/install.sh" = {
+      source = ./install.sh;
+      mode   = "0700";
+    };
+
+    "installer/configuration.nix" = {
+      source = ./configuration.nix;
+      mode   = "0660";
+    };
+  };
+}
