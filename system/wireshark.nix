e7308a3069cbbaad59a4cd84d379c2a416069e8f
Petr Kozorezov <petr.kozorezov@gmail.com>
2021-02-20 07:23:15 +0300

    enable wireshark

--- system/wireshark.nix
+++ system/wireshark.nix
@@ -1,5 +1,8 @@
-{ ... }:
+{ pkgs, ... }:
 {
-  programs.wireshark.enable = false;
+  programs.wireshark = {
+    enable = true;
+    package = pkgs.wireshark;
+  };
   users.users.petrkozorezov.extraGroups  = [ "wireshark" ];
 }
