32190f13752237dc051c1f458923fbfd2420cfd5
Petr Kozorezov <petr.kozorezov@gmail.com>
2022-02-02 18:18:14 +0300

    add managing xdg with home-manager

--- home/profiles/petrkozorezov/desktop/xdg.nix
+++ home/profiles/petrkozorezov/desktop/xdg.nix
@@ -1,18 +1,46 @@
 { pkgs, ... }:
 {
   xdg = {
-    # TODO
     # https://wiki.archlinux.org/title/Default%20applicationsq
-    # mimeo --mimeapps-list
-    # mimeApps = {
-    #   enable = true;
+    userDirs = {
+      enable            = true;
+      createDirectories = true;
+    };
+    desktopEntries = {
+       # firefox = {
+       #   name        = "Firefox";
+       #   genericName = "Web Browser";
+       #   exec        = "firefox %U";
+       #   terminal    = false;
+       #   categories  = [ "Application" "Network" "WebBrowser" ];
+       #   mimeType    = [ "text/html" "text/xml" ];
+       # };
+    };
+    mime.enable = true;
+    mimeApps = {
+      enable = true;
+      defaultApplications             = {
+        "x-scheme-handler/http"         = "firefox.desktop";
+        "x-scheme-handler/https"        = "firefox.desktop";
+        "x-scheme-handler/ftp"          = "firefox.desktop";
+        "x-scheme-handler/chrome"       = "firefox.desktop";
+        "text/html"                     = "firefox.desktop";
+        "application/x-extension-htm"   = "firefox.desktop";
+        "application/x-extension-html"  = "firefox.desktop";
+        "application/x-extension-shtml" = "firefox.desktop";
+        "application/xhtml+xml"         = "firefox.desktop";
+        "application/x-extension-xhtml" = "firefox.desktop";
+        "application/x-extension-xht"   = "firefox.desktop";
 
-    #   defaultApplications = {};
-    # };
-    # userDirs = {
-    #   enable = true;
-    #   createDirectories = true;
-    # };
+        "text/plain"                = "sublime_text.desktop";
+        "text/tab-separated-values" = "sublime_text.desktop";
+        "application/text"          = "sublime_text.desktop";
+        "application/octet-stream"  = "sublime_text.desktop";
+
+        "x-scheme-handler/tg"           = "userapp-Telegram Desktop-1FKGD1.desktop"; # FIXME
+        "x-scheme-handler/mailto"       = "chromium-browser.desktop"; # FIXME
+      };
+    };
   };
 
   home.packages = with pkgs; [ xdg_utils mimeo ];
