648eeaee9b0b6c1617eaccc38b64c3ae7b453bf6
Petr Kozorezov <petr.kozorezov@gmail.com>
2022-06-02 20:42:10 +0300

    update sublime config

--- home/profiles/petrkozorezov/desktop/subl.nix
+++ home/profiles/petrkozorezov/desktop/subl.nix
@@ -5,18 +5,23 @@ let
   userPath          = "${packagesPath}/User";
   localPath         = "${sublPath}/Local";
   installedPkgsPath = "${sublPath}/Installed Packages";
+  packageStorage    = ".cache/${sublPath}/Package Storage";
   sublSetting       = {
-    auto_complete                     = true;
-    auto_match_enabled                = false;
-    color_scheme                      = "Packages/MonokaiFree/MonokaiFree.tmTheme";
+    # ui
+    theme                             = "Afterglow.sublime-theme";
+    tabs_small                        = true;
+    sidebar_size_13                   = true;
+    color_scheme                      = "Packages/Theme - Afterglow/Afterglow-monokai.tmTheme";
     font_face                         = "JetBrains Mono";
     font_size                         = 14;
+
+    auto_complete                     = true;
+    auto_match_enabled                = false;
     ignored_packages                  = [];
     tab_size                          = 2;
-    theme                             = "Soda Dark.sublime-theme";
     translate_tabs_to_spaces          = true;
     trim_trailing_white_space_on_save = true;
-    ensure_newline_at_eof_on_save     = false;
+    ensure_newline_at_eof_on_save     = true;
     update_check                      = false;
     vintage_start_in_command_mode     = true;
     copy_with_empty_selection         = true;
@@ -26,6 +31,7 @@ let
       [
         "*.jpg" "*.jpeg" "*.png" "*.gif" "*.ttf" "*.tga" "*.dds" "*.ico" "*.eot" "*.pdf" "*.swf" "*.jar" "*.zip" "*.beam"
       ];
+    node = pkgs.nodejs;
   };
   sublKeymap =
     [
@@ -38,34 +44,54 @@ let
       { keys = ["ctrl+shift++"]; command = "move_to_group" ; args = { group        = 1   ; }; }
       { keys = ["ctrl+shift+]"]; command = "move_to_group" ; args = { group        = 2   ; }; }
       { keys = ["ctrl+shift+a"]; command = "align_tab"     ; args = { live_preview = true; }; }
+      { keys = ["ctrl+alt+right"]; command = "lsp_symbol_definition"  ; } # alt+'-' -- back
+      { keys = ["ctrl+alt+left" ]; command = "lsp_symbol_references"  ; }
+      { keys = ["ctrl+alt+up"   ]; command = "lsp_code_actions"       ; }
+      { keys = ["ctrl+alt+down" ]; command = "lsp_hover"              ; }
     ];
   packages = {
     bootstrapped        = true;
     in_process_packages = [];
-    installed_packages  =
-    [
-      "MonokaiFree"
-      "Theme - Soda"
+    installed_packages  = [
+      # ui
+      "Theme - Afterglow"
+      "SideBarEnhancements"
+      "WordHighlight"
+      "Terminus"
+
+      # editing
       "AlignTab"
-      "Elixir"
-      "GitGutter"
-      "Nix"
+      "ChangeQuotes"
       "EditorConfig"
       "Pretty JSON"
+      "Text Pastry"
+
+      # languages
+      "LSP"
+      # "LSP-file-watcher-chokidar" # periodically eats too much CPU
+      # "LSP-bash"
+      "ElixirSyntax"
+      "LSP-elixir"
+      "Nix"
       "Protocol Buffer Syntax"
-      "SideBarEnhancements"
-      "SublimeGit"
       "ThriftSyntax"
       "TOML"
-      "WordHighlight"
-      "LSP"
       "Dockerfile Syntax Highlighting"
+      "LSP-dockerfile"
       "Coq"
-      "TypeScript"
+      "Idris Syntax"
+      "LSP-typescript"
+      "LSP-eslint"
+      "Rust Enhanced"
+      "LSP-rust-analyzer"
       "Ethereum"
-      # doesn't work
-      #"Markdown Preview"
-      #"TabNine"
+      "MarkdownEditing"
+      # "LSP-ltex-ls"
+
+      # VCS
+      "SublimeGit"
+      "GitGutter"
+      "FileDiffs"
     ];
   };
   lsp = {
@@ -77,12 +103,6 @@ let
             command  = [ "${pkgs.erlang-ls}/bin/erlang_ls" "--transport" "stdio" "" ];
             selector = "source.erlang";
           };
-        rust-analyzer =
-          {
-            enabled  = true;
-            command  = [ "${pkgs.rust-analyzer}/bin/rust-analyzer" ];
-            selector = "source.rust";
-          };
       };
     initialize_timeout = 30;
     log_debug    = false;
@@ -90,31 +110,61 @@ let
     log_stderr   = false;
     log_payloads = false;
   };
+  lspTypescript.settings = {
+    "typescript.format.semicolons" = "remove";
+  };
   # rustFmt = {
   #   format_on_save = true;
   #   executable     = ["${pkgs.rustfmt}/bin/rustfmt"];
   # };
 in
 {
-  xdg.configFile = {
-    "${userPath}/Preferences.sublime-settings".text =
-      builtins.toJSON sublSetting;
-    "${userPath}/Default (Linux).sublime-keymap".text =
-      builtins.toJSON sublKeymap;
-    "${userPath}/LSP.sublime-settings".text =
-      builtins.toJSON lsp;
-    "${userPath}/Package Control.sublime-settings".text =
-      builtins.toJSON packages;
-    # "${userPath}/RustFmt.sublime-settings".text =
-    #   builtins.toJSON rustFmt;
-    # TODO pass by config
-    "${localPath}/License.sublime_license".source = config.zoo.secrets.filesPath + "/sublime.license";
-    "${installedPkgsPath}/Package Control.sublime-package".source =
-      builtins.fetchurl {
-        name   = "sublime-package-control";
-        url    = "https://packagecontrol.io/Package%20Control.sublime-package";
-        sha256 = "0ffi4qn0ndhqphnggy02mhzrqvv5n8c57f23rn44rj1l9ha3fyc1";
-      };
+  xdg = {
+    configFile = {
+      "${userPath}/Preferences.sublime-settings".text =
+        builtins.toJSON sublSetting;
+      "${userPath}/Default (Linux).sublime-keymap".text =
+        builtins.toJSON sublKeymap;
+      "${userPath}/LSP.sublime-settings".text =
+        builtins.toJSON lsp;
+      "${userPath}/Package Control.sublime-settings".text =
+        builtins.toJSON packages;
+      "${userPath}/LSP-typescript.sublime-settings".text =
+        builtins.toJSON lspTypescript;
+
+      # "${userPath}/RustFmt.sublime-settings".text =
+      #   builtins.toJSON rustFmt;
+      # TODO pass by config
+      "${localPath}/License.sublime_license".source = config.zoo.secrets.filesPath + "/sublime.license";
+      "${installedPkgsPath}/Package Control.sublime-package".source =
+        builtins.fetchurl {
+          name   = "sublime-package-control";
+          url    = "https://packagecontrol.io/Package%20Control.sublime-package";
+          sha256 = "0ffi4qn0ndhqphnggy02mhzrqvv5n8c57f23rn44rj1l9ha3fyc1";
+        };
+    };
   };
-  home.packages = with pkgs; [ sublime3 jetbrains-mono ];
+
+  home.file = {
+    # TODO other LSP packages
+    "${packageStorage}/LSP-rust-analyzer/rust-analyzer".source = "${pkgs.rust-analyzer}/bin/rust-analyzer";
+  };
+  home.packages = with pkgs; [
+    jetbrains-mono
+
+    erlang
+    nodejs
+    elixir
+    rustc cargo
+    mdl # markdownlint
+    shellcheck
+    adoptopenjdk-jre-bin
+
+    (sublime4.overrideAttrs ({ propagatedBuildInputs ? [], ... }: {
+      propagatedBuildInputs =
+        propagatedBuildInputs ++ [];
+    }))
+
+    # oni2 # just to look on it
+  ];
 }
