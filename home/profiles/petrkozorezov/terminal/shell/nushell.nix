fd3dd0690d59e3044baadb3a060668f125aaca5a
Petr Kozorezov <petr.kozorezov@gmail.com>
2024-01-11 12:30:10 +0300

    add nushell aliases

--- home/profiles/petrkozorezov/terminal/shell/nushell.nix
+++ home/profiles/petrkozorezov/terminal/shell/nushell.nix
@@ -1,30 +1,40 @@
 { pkgs, ... }: {
-  # TODO
-  #  - git aliases
+  # TODO:
+  #  - git aliases (mb https://gist.github.com/spitfire05/103324f015d12c661f5da9e329852ad0 ?)
+  #  - nu_scripts (eg git/gh/nix completions)
+  #  - skim integration # https://github.com/nushell/nushell/issues/1275
+  #  - plugins
   programs = {
     nushell = {
-      enable = true;
+      enable  = true;
+      package = pkgs.nushellFull;
       shellAliases = {
-        "." = "ls";
-        "l" = "ls";
-        "cat"   = "${pkgs.bat}/bin/bat";
-        "ns"    = "nix search mynixos";
-        "nb"    = "nix build";
-        "cloc"  = "${pkgs.tokei}/bin/tokei";
-        # "noidle" = ''
-        #   echo "Press Ctrl+C to interrupt..."
-        #   systemd-inhibit --what=idle --mode=block --who="noidle" --why="Manual prevent from sleep" sleep infinity
-        # '';
-          # nsh() {
-          #   nix shell "mynixos#$1"
-          # }
-          # fix-touchpad() {
-          #   sudo rmmod bcm5974 && sudo modprobe bcm5974 && dmesg | tail
-          # }
+        "."    = "ls";
+        "l"    = "ls";
+        "cat"  = "${pkgs.bat}/bin/bat";
+        "ns"   = "nix search mynixos";
+        "nb"   = "nix build";
+        "cloc" = "${pkgs.tokei}/bin/tokei";
       };
       extraConfig = ''
         $env.config = {
           show_banner: false
+        }
+
+        def noidle [] {
+          echo "Press Ctrl+C to interrupt..."
+          systemd-inhibit --what=idle --mode=block --who="noidle" --why="Manual prevent from sleep" sleep infinity
+        }
+
+        def fix-touchpad [] {
+          sudo rmmod bcm5974
+          sudo modprobe bcm5974
+          dmesg | tail
+        }
+
+        # TODO args (https://github.com/nushell/nushell/discussions/7457)
+        def nsh [ pkg: string ] {
+          nix shell $'mynixos#($pkg)'
         }
       '';
     };
