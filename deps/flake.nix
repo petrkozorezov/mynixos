3c06d0083dcd29c035440810c47c44aa62d5e26b
Petr Kozorezov <petr.kozorezov@gmail.com>
2025-01-25 14:49:42 +0300

    get rid of devenv-nixpkgs

--- deps/flake.nix
+++ deps/flake.nix
@@ -7,7 +7,6 @@
                nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11"       ;
           home-manager.url = "github:rycee/home-manager/release-24.11";
                    nur.url = "github:nix-community/NUR"               ;
-        devenv-nixpkgs.url = "github:cachix/devenv-nixpkgs/rolling"   ;
                 devenv.url = "github:cachix/devenv/v1.3.1"            ;
              deploy-rs.url = "github:serokell/deploy-rs"              ;
            flake-utils.url = "github:numtide/flake-utils"             ;
@@ -17,8 +16,9 @@
 
           home-manager.inputs.nixpkgs.follows = "nixpkgs";
                    nur.inputs.nixpkgs.follows = "nixpkgs";
-                devenv.inputs.nixpkgs.follows = "devenv-nixpkgs";
+                devenv.inputs.nixpkgs.follows = "nixpkgs";
              deploy-rs.inputs.nixpkgs.follows = "nixpkgs";
+           flake-utils.inputs.nixpkgs.follows = "nixpkgs";
                    dns.inputs.nixpkgs.follows = "nixpkgs";
     nix-index-database.inputs.nixpkgs.follows = "nixpkgs";
                  fenix.inputs.nixpkgs.follows = "nixpkgs";
